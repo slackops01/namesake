74dc4a103288ec0427915c0183e3ce3846cec953
SlackOps01 <olanrewajusholarin5@gmail.com>
2025-10-19 10:52:05 +0100

    fixes made

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],
+    allow_origins = ["https://www.olanrewajusholarin.com/"],
     allow_methods = ["*"],
     allow_credentials = True,
     allow_headers = ["*"]
@@ -19,4 +19,4 @@ def get_compliment():
     with open("compliments.json", "r") as file:
         file_data = json.load(file)
         compliment: str = random.choice(file_data).strip()
-        return {f"Heyy Tomisin, {compliment}"}
+        return {"message": f"Heyy Tomisin, {compliment}"}
